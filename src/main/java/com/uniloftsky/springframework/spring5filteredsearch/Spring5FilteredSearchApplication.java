60c78d8afe904cd439d0ae88f595eae710053767
uniloftsky <uniloftsky@gmail.com>
2021-03-14 16:19:53 +0200

    project added

---
+++ src/main/java/com/uniloftsky/springframework/spring5filteredsearch/Spring5FilteredSearchApplication.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5filteredsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5FilteredSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5FilteredSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniloftsky/springframework/spring5filteredsearch/Spring5FilteredSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5filteredsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5FilteredSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
